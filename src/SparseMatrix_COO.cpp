0ae2343a15fe0c72ab69c39639ea0e0885729901
NeilFAsh <nfash@umich.edu>
2025-10-22 18:42:03 -0400

    Completed getValue and setValue function implementations

--- src/SparseMatrix_COO.cpp
+++ src/SparseMatrix_COO.cpp
@@ -24,16 +24,51 @@ namespace SpMV
 
     template <class fp_type>
     void SparseMatrix_COO<fp_type>::setValue(const size_t i, const size_t j, fp_type val) :
-        SparseMatrix<fp_type>::setValue(i, j, val)
+        //SparseMatrix<fp_type>::setValue(i, j, val)
     {
         cout << "Hello from COO setValue" << endl;
+        
+        assert(i < this->_nrow);
+        assert(j < this->_ncol);
+        assert(this->_state == MatrixState::assembled);
+
+        this->_state = MatrixState::initialized;
+        
+        // Check if the i,j are present in _buildCoeff
+        // if yes, replace
+        // if not, add
+        // afterwards, reassemble
+        // Actually this all could be replaced with a call to 
+        // SparseMatrix<fp_type>::setValue(i, j, val)
+        auto it = this->_buildCoeff.find(std::make_pair(i,j));
+        if (it != this->_buildCoeff.end()) {
+            it->second = val;
+        } else {
+            this->_buildCoeff[std::make_pair(i,j)] = val;
+        }
+        // Re-assemble the matrix
+        assemble();
+        assert(this->_state == MatrixState::assembled);
     }
 
     template <class fp_type>
     fp_type SparseMatrix_COO<fp_type>::getValue(const size_t i, const size_t j) :
-        SparseMatrix<fp_type>::getValue(i, j)
+        //SparseMatrix<fp_type>::getValue(i, j)
     {
         cout << "Hello from COO getValue" << endl;
+        assert(this->_state >= MatrixState::assembled);
+        assert(i < this->_nrow);
+        assert(j < this->_ncol);
+        
+        // Traverse the nz values and find the value at (i,j)
+        for (size_t idx = 0; idx < this->_numnz; idx++)
+        {
+            if (this->_idx_row[idx] == i && this->_idx_col[idx] == j)
+            {
+                return this->_aij[idx];
+            }
+        }
+        return 0.0;
     }
 }
 
