b5e1b7e98e71698e71a083b6da712b5f9faad6ed
Choco Li <tianycli@umich.edu>
2025-10-22 14:58:36 -0400

    Implemented a matvec method for SparseMatrix_COO class; included protected attributes in the header file as placeholder for the COO constructor.

--- include/SparseMatrix_COO.hpp
+++ include/SparseMatrix_COO.hpp
@@ -4,18 +4,32 @@
 #include <iostream>
 #include <stddef.h>
 #include "SparseMatrix.hpp"
+#include <vector>
 
 namespace SpMV
 {
     template <class fp_type>
     class SparseMatrix_COO : public SparseMatrix<fp_type>
     {
+        protected: 
+            std::vector<fp_type> aij;
+            std::vector<size_t> idx_col;
+            std::vector<size_t> idx_row;
+
         public:
             SparseMatrix_COO(const size_t nrows, const size_t ncols);
             void assemble();
 
             void    setValue(const size_t i, const size_t j, fp_type val);
             fp_type getValue(const size_t i, const size_t j);
+
+            /**
+             * @brief Apply the sparse matrix to input vector x to get b = Ax.
+             * 
+             * @param x input vector to be left multiplied by matrix
+             * @return std::vector<fp_type> 
+             */
+            std::vector<fp_type> matvec(std::vector<fp_type> x);
     };
 }
 
