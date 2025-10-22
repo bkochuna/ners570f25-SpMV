3353155ddab7dd52dae2499eb307ae452c9bd64f
Brandon Hardy <59355744+b-k-hardy@users.noreply.github.com>
2025-10-22 11:26:36 -0400

    added ELL-specific class attributes with default values

--- include/SparseMatrix_ELL.hpp
+++ include/SparseMatrix_ELL.hpp
@@ -2,9 +2,16 @@
 #define __SPMV_SparseMatrix_ELL__
 
 #include "SparseMatrix.hpp"
+#include <vector>
 
 namespace SpMV {
 template <class fp_type> class SparseMatrix_ELL : public SparseMatrix<fp_type> {
+protected:
+  size_t _maxNnzPerRow = 0;
+  std::vector<fp_type> _values{};
+  std::vector<int> _colIndices{}; // signed to allow -1 for padding (if desired
+                                  // by matVec implementation)
+
 public:
   SparseMatrix_ELL(const size_t nrows, const size_t ncols);
   ~SparseMatrix_ELL();
