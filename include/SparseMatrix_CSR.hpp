d74f0f6b102862107779c4c807b5c31f662c9d39
Nazmul Hossain <nazmulh@umich.edu>
2025-10-22 13:51:55 -0400

    The skeleton was changed

--- include/SparseMatrix_CSR.hpp
+++ include/SparseMatrix_CSR.hpp
@@ -1,15 +1,15 @@
-#ifndef __SPMV_SparseMatrix_COO__
-#define __SPMV_SparseMatrix_COO__
+#ifndef __SPMV_SparseMatrix_CSR__
+#define __SPMV_SparseMatrix_CSR__
 
 #include "SparseMatrix.hpp"
 
 namespace SpMV
 {
     template <class fp_type>
-    class SparseMatrix_COO : public SparseMatrix<fp_type>
+    class SparseMatrix_CSR : public SparseMatrix<fp_type>
     {
         public:
-            SparseMatrix_COO(const size_t nrows, const size_t ncols);
+            SparseMatrix_CSR(const size_t nrows, const size_t ncols);
             void assemble();
     };
 }
--- src/SparseMatrix_CSR.cpp
+++ src/SparseMatrix_CSR.cpp
@@ -1,4 +1,4 @@
-#include "SparseMatrix_COO.hpp"
+#include "SparseMatrix_CSR.hpp"
 
 #include <iostream>
 #include <stddef.h>
@@ -8,22 +8,22 @@ using namespace std;
 namespace SpMV
 {
     template <class fp_type>
-    SparseMatrix_COO<fp_type>::SparseMatrix_COO(const size_t nrows, const size_t ncols) :
+    SparseMatrix_CSR<fp_type>::SparseMatrix_CSR(const size_t nrows, const size_t ncols) :
         SparseMatrix<fp_type>::SparseMatrix(nrows, ncols)
     {
-        cout << "Hello from COO Constructor" << endl;
+        cout << "Hello from CSR Constructor" << endl;
     }
 
     template <class fp_type>
     void SparseMatrix_COO<fp_type>::assemble()
     {
-        cout << "Hello from COO assemble" << endl;
+        cout << "Hello from CSR assemble" << endl;
 
-        //This routine needs to convert _buildCoeff into the COO storage format.
+        //This routine needs to convert _buildCoeff into the CSR storage format.
     }
 }
 
 // Need to declare the concrete templates within the library for
 // use in code that links to libspmv
-template class SpMV::SparseMatrix_COO<float>;
-template class SpMV::SparseMatrix_COO<double>;
+template class SpMV::SparseMatrix_CSR<float>;
+template class SpMV::SparseMatrix_CSR<double>;
