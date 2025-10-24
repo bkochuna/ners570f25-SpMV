dd3e5d7e45435eec6db81590d98f36245bfdb794
Ethan Stout <enstout@umich.edu>
2025-10-23 23:28:32 -0400

    Added some extra tests for robustness, added some bug fixes (semicolons and syntax)

--- tests/SparseMatrix_COO_Storage_Test.cpp
+++ tests/SparseMatrix_COO_Storage_Test.cpp
@@ -1,13 +1,16 @@
 // Make protected attributes public JUST for this test suite (_idx_col, _idx_row, _aij)
+// Kind of janky but should ultimately use friend to access the attributes, once everyones contributions are in
 #define protected public
 #include <SpMV.hpp>
-#undef protected 
+#undef protected
 
 #include <vector>
 #include <algorithm>
+#include <set>
 
 #include "unit_test_framework.hpp"
 
+using namespace SpMV;  
 
 
 // Minimal test to test that _idx_col, _idx_row, and _aij are all consistent with each other and the test matrix
@@ -16,7 +19,6 @@ TEST_CASE(test_mat_features)
 {
 	// Initialize a simple 3x3 COO matrix of type T
 	using fp_type = T;
-	fp_type eps = 1e-12; // Assertion tolerance
 	SparseMatrix_COO<fp_type> A(3,3);
 
 	// Set two diagonal, and two off-diagonal terms
@@ -47,6 +49,18 @@ TEST_CASE(test_mat_features)
 	ASSERT(col_max == 2);
 	ASSERT(col_min == 0);
 
+	// Check for idempotence
+	size_t nrow_prev = A._idx_row.size();
+	size_t ncol_prev = A._idx_col.size();
+	size_t naij_prev = A._aij.size();
+
+	A.assemble(); // Call assemble again
+
+	ASSERT(A._idx_row.size() == nrow_prev);
+	ASSERT(A._idx_col.size() == ncol_prev);
+	ASSERT(A._aij.size() == naij_prev);
+
+
 }
 
 // Minimal test to test setting diagonal and off-diagonal terms
@@ -55,7 +69,7 @@ TEST_CASE(test_assemble)
 {
 	// Initialize a simple 3x3 COO matrix of type T
 	using fp_type = T;
-	fp_type eps = 1e-12; // Assertion tolerance
+	const fp_type eps = 1e-6; // Assertion tolerance
 	SparseMatrix_COO<fp_type> A(3,3);
 
 	// Set two diagonal, and two off-diagonal terms
@@ -78,6 +92,33 @@ TEST_CASE(test_assemble)
 	ASSERT_NEAR(A.getValue(1, 2), 0.0, eps);
 	ASSERT_NEAR(A.getValue(2, 1), 0.0, eps);
 
+	
+	// Check for uniqueness
+	std::set<std::pair<size_t, size_t>> prev_ind;
+	for (size_t k = 0; k < A._aij.size(); ++k) {
+		ASSERT(prev_ind.insert({A._idx_row[k], A._idx_col[k]}).second);	
+	}
+
+	
+	// Check for idempotence
+	A.assemble(); // Reassemble
+
+	ASSERT_NEAR(A.getValue(0, 0), 1.0, eps);
+	ASSERT_NEAR(A.getValue(0, 1), 2.0, eps);
+	ASSERT_NEAR(A.getValue(1, 0), 3.0, eps);
+	ASSERT_NEAR(A.getValue(2, 2), 4.0, eps);
+	ASSERT_NEAR(A.getValue(1, 1), 0.0, eps);
+	ASSERT_NEAR(A.getValue(0, 2), 0.0, eps);
+	ASSERT_NEAR(A.getValue(2, 0), 0.0, eps);
+	ASSERT_NEAR(A.getValue(1, 2), 0.0, eps);
+	ASSERT_NEAR(A.getValue(2, 1), 0.0, eps);
+
+	// Check overwrite
+	A.setValue(2, 2, 5.0);
+	A.assemble();
+	ASSERT_NEAR(A.getValue(2, 2), 5.0, eps);
+
+
 }
 
 
@@ -87,8 +128,6 @@ TEST_CASE(test_deassemble)
 {
 	// Initialize a simple 3x3 COO matrix of type T
 	using fp_type = T;
-	fp_type eps = 1e-12; // Assertion tolerance
-	fp_type default_val = 0.0 // Default sparse value of 0.0
 	SparseMatrix_COO<fp_type> A(3,3);
 
 	// Set two diagonal, and two off-diagonal terms
@@ -98,8 +137,8 @@ TEST_CASE(test_deassemble)
 	A.setValue(2, 2, 4.0);
 
 	// Assemble using implemented COO assemble function
-	A.assemble()
-	A.deassemble()
+	A.assemble();
+	A.deassemble();
 
 	// The deassembled values should just be empty if there are no values
 	ASSERT(A._idx_row.size() == 0);
@@ -117,8 +156,7 @@ TEST_CASE(test_reassemble)
 {
 	// Initialize a simple 3x3 COO matrix of type T
 	using fp_type = T;
-	fp_type eps = 1e-12; // Assertion tolerance
-	fp_type default_val = 0.0
+	const fp_type eps = 1e-6; // Assertion tolerance
 	SparseMatrix_COO<fp_type> A(3,3);
 
 	// Set two diagonal, and two off-diagonal terms
@@ -128,14 +166,17 @@ TEST_CASE(test_reassemble)
 	A.setValue(2, 2, 4.0);
 
 	// Assemble using implemented COO assemble function
-	A.assemble()
-	A.deassemble()
+	A.assemble();
+	A.deassemble();
 
 	// Put new values into _BuildCoeffs
 	A.setValue(0, 0, 5.0);
 	A.setValue(0, 1, 6.0);
 	A.setValue(1, 0, 7.0);
 	A.setValue(2, 2, 8.0);
+
+	// Reassemble
+	A.assemble();
 
 	// Test that the reassembled values are what is expected
 	ASSERT_NEAR(A.getValue(0, 0), 5.0, eps);
@@ -148,6 +189,12 @@ TEST_CASE(test_reassemble)
 	ASSERT_NEAR(A.getValue(1, 2), 0.0, eps);
 	ASSERT_NEAR(A.getValue(2, 1), 0.0, eps);
 
+
+	// Check for uniqueness
+	std::set<std::pair<size_t, size_t>> prev_ind;
+	for (size_t k = 0; k < A._aij.size(); ++k) {
+		ASSERT(prev_ind.insert({A._idx_row[k], A._idx_col[k]}).second);	
+	}
 
 }
 
