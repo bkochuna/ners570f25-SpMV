c279ddbceafd2a7c285e69b526d5829d59d77678
Brandon Hardy <59355744+b-k-hardy@users.noreply.github.com>
2025-10-30 14:20:39 -0400

    response to review: better comments and fixed naming convention for `_paddingValue`

--- include/SparseMatrix_ELL.hpp
+++ include/SparseMatrix_ELL.hpp
@@ -4,18 +4,62 @@
 #include "SparseMatrix.hpp"
 #include <vector>
 
+/**
+ * ELLPACK (ELL) sparse matrix storage template declaration.
+ *
+ * Detailed description: Header declaring the ELL sparse matrix format. Inherits
+ * from SparseMatrix. This format comes from ELLPACK, an elliptical PDE solver.
+ * It is particularly efficient for SIMD instructions or with GPU
+ * parallelization. The implementation should work at minimum for both `double`
+ * and `float`, hence the templating via `fp_type` (floating point type)
+ */
+
 namespace SpMV {
 template <class fp_type> class SparseMatrix_ELL : public SparseMatrix<fp_type> {
 protected:
+  /**
+   * Maximum number of non-zero entries per row.
+   */
   size_t _maxNnzPerRow = 0;
-  int paddingValue = -1; // Value to use to indicate padding in column indices
+
+  /**
+   * Value used to indicate padding in _colIndices{} for ELL matrix format.
+   * Default of -1
+   */
+  int _paddingValue = -1; //
+
+  /**
+   * Flattened storage for non-zero values. Junk values where padding indicated.
+   */
   std::vector<fp_type> _values{};
-  std::vector<int> _colIndices{}; // signed to allow -1 for padding (if desired
-                                  // by matVec implementation)
+
+  /**
+   * Column indices for each stored value.
+   * Signed type allows using `paddingValue` (-1) to mark unused slots.
+   */
+  std::vector<int> _colIndices{};
 
 public:
+  /**
+   * Construct an empty ELL matrix with the given size.
+   *
+   * @param nrows Number of rows in the matrix.
+   * @param ncols Number of columns in the matrix.
+   *
+   * `_values` and `_colIndices` are empty and
+   * `_maxNnzPerRow` is zero until `assemble()` is called.
+   */
   SparseMatrix_ELL(const size_t nrows, const size_t ncols);
+
+  /**
+   * Destroy the matrix and free resources.
+   */
   ~SparseMatrix_ELL();
+
+  /**
+   * Assemble the ELL matrix from the inherited COO-like
+   * representation
+   */
   void assemble();
 };
 } // namespace SpMV
