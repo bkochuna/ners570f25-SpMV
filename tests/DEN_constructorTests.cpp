6aeaf72bbae24f3f4574eb4e52d309430b4a0340
= <=>
2025-10-30 18:16:26 -0400

    Add constructor tests and edited to test required types float/double. Also changed the test to use the accessors which are defined.

--- tests/DEN_constructorTests.cpp
+++ tests/DEN_constructorTests.cpp
@@ -9,11 +9,11 @@ TEST_CASE(test_DEN_constructor_double){
     SpMV::SparseMatrix_DEN<double> A(5,6);
 
     // check that the values assigned into the object are correct
-    ASSERT(A.nrows == 5);
-    ASSERT(A.ncols == 6);
+    ASSERT(A.nrows() == 5);
+    ASSERT(A.ncols() == 6);
 
     // check that object state is initialized
-    ASSERT(A._state == SpMV::MatrixState::initialized)
+    ASSERT(A.getState() == SpMV::MatrixState::initialized);
 }
 
 TEST_CASE(test_DEN_destructor_double){
@@ -24,21 +24,21 @@ TEST_CASE(test_DEN_destructor_double){
     // If test fails, a segmentation fault will occur
 }
 
-TEST_CASE(test_DEN_constructor_int){
+TEST_CASE(test_DEN_constructor_float){
     // test creation of new object
-    SpMV::SparseMatrix_DEN<int> A(5,6);
+    SpMV::SparseMatrix_DEN<float> A(5,6);
 
     // check that the values assigned into the object are correct
-    ASSERT(A.nrows == 5);
-    ASSERT(A.ncols == 6);
+    ASSERT(A.nrows() == 5);
+    ASSERT(A.ncols() == 6);
 
     // check that object state is initialized
-    ASSERT(A._state == SpMV::MatrixState::initialized)
+    ASSERT(A.getState() == SpMV::MatrixState::initialized);
 }
 
-TEST_CASE(test_DEN_destructor_int){
+TEST_CASE(test_DEN_destructor_float){
     // test that object can be deleted
-    SpMV::SparseMatrix_DEN<int>* testing = new SpMV::SparseMatrix_DEN<int>(5,5); 
+    SpMV::SparseMatrix_DEN<float>* testing = new SpMV::SparseMatrix_DEN<float>(5,5); 
     delete testing;
     // Test will fail if the object can not be deleted
     // If test fails, a segmentation fault will occur
@@ -48,9 +48,9 @@ TEST_SUITE(DEN_con_des_suite){
     TEST(test_DEN_constructor_double);
     TEST(test_DEN_destructor_double);
 
-    // run tests again with integers
-    TEST(test_DEN_constructor_int);
-    TEST(test_DEN_destructor_int);
+    // run tests again with floats
+    TEST(test_DEN_constructor_float);
+    TEST(test_DEN_destructor_float);
 }
 
 int main(){
