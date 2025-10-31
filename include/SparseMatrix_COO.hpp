aa9051a9d3b2b128271746b170f391a117c5271c
ncmaclachlan <ncmaclachlan@gmail.com>
2025-10-31 14:39:21 -0400

    moved COO testing getters to COO header to avoid merge conflicts

--- include/SparseMatrix_COO.hpp
+++ include/SparseMatrix_COO.hpp
@@ -11,6 +11,15 @@ namespace SpMV
         public:
             SparseMatrix_COO(const size_t nrows, const size_t ncols);
             void assemble();
+    
+    
+            // Getters for testing
+            size_t nrows() const { return this->_nrows; }
+            size_t ncols() const { return this->_ncols; }
+            size_t numnz() const { return this->_numnz; }
+            MatrixState getState() const { return this->_state; }
+            bool buildCoeffEmpty() const { return this->_buildCoeff.empty(); }
+            size_t buildCoeffSize() const { return this->_buildCoeff.size(); }
     };
 }
 
