3a4617df8091cd53b65cc576e96475ca618509e0
Ellen Meyer <ecmeyer@umich.edu>
2025-10-31 15:38:19 -0400

    Another variable name fix for COO

--- src/SparseMatrix_COO.cpp
+++ src/SparseMatrix_COO.cpp
@@ -116,7 +116,7 @@ namespace SpMV
         //For loop to perform matvec
         for (size_t i=0; i < (this->_numnz) ; i++) {
             b[this->_idx_row[i]] += this->_aij[i] * x[this->_idx_col[i]]; 
-            b[this->_idx_row[i]] += this->aij[i] * x[this->_idx_col[i]]; 
+            b[this->_idx_row[i]] += this->_aij[i] * x[this->_idx_col[i]]; 
         }
 
         return b;
