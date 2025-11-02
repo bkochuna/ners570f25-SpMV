a67bb7a0df2730dcb92815fbd45e04ccfa7e9f89
spencb-bit <spencb@umich.edu>
2025-11-02 14:13:27 -0500

    Add files via upload
    minor changes associated with .hpp file for viewDEN.cpp for namespace inclusion

--- include/viewDEN.hpp
+++ include/viewDEN.hpp
@@ -1,29 +1,11 @@
 #ifndef VIEW_DEN_HPP
-
 #define VIEW_DEN_HPP
 
+#include "DenseMatrix.hpp"
 
-
-#include "DenseMatrix.hpp"  // Use the existing class definition (PLEASE CHANGE NAME IF NEEDED)
-
-#include <iostream>
-
-#include <iomanip>
-
-#include <sstream>
-
-
-
-// This header only declares the viewDEN method implementation
-
-// for an already-defined DenseMatrix class. (no DEN matrix storage method exists at time of writing) 
-
-
-
-void DenseMatrix::viewDEN() const;
-
-
+namespace SpMV
+{
+    void viewDEN(const DenseMatrix& A);
+}
 
 #endif // VIEW_DEN_HPP
-
-
