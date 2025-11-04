ee9b1e177408526213d1e5add510193c49575247
Brendan Kochunas <bkochuna@umich.edu>
2025-11-04 09:44:01 -0500

    WIP: Updates SparseMatrix base class with several members
    Description:
    This is a Work In Progress commit. It does not build.
    This updates the SparseMatrix base class with several items
     * defines another enumerated type for the MatrixFormat
     * defines a new private attribute to store the MatrixFormat (this is immutable)
     * defines two protected abstract functions to be implemented on
       derived classes. _disassembl() and _getValueConcrete
     * defines several oneline accessor functions for attributes
       defined on the base class
     * defines abstract method matvec on the base class to be implemented
       on the derived classes.
     * Adds the const attribute to the getValue method. This means the
       method does not update the object (i.e. this-> does not appear
       on the left hand side of any assignment operatores within the method).

--- include/SparseMatrix.hpp
+++ include/SparseMatrix.hpp
@@ -10,27 +10,44 @@ using namespace std;
 namespace SpMV
 {
 
+    // Enumerations for matrix state to support builder pattern
     enum MatrixState {undefined, initialized, building, assembled};
+
+    // Enumerations for Matrix Format
+    enum MatrixFormat {UDF, DEN, COO, CSR, ELL, JDS};
 
     template <class fp_type>
     class SparseMatrix
     {
-    protected:
+    private:
         MatrixState _state = MatrixState::undefined;
              size_t _nrows = 0;
              size_t _ncols = 0;
              size_t _numnz = 0;
 
+        const MatrixFormat _fmt;
+             
+
+    protected:
         std::map<std::pair<size_t, size_t>, fp_type> _buildCoeff;
         
-
+        virtual void _disassemble() =0;
+        virtual fp_type _getValueConcrete(const size_t i, const size_t j) const =0;
     public:
         SparseMatrix(const size_t nrows, const size_t ncols);
         virtual ~SparseMatrix();
 
+        inline void         setState(MatrixState S) { this->_state = S; };
+        inline MatrixState  getState()          const noexcept { return _state; };
+        inline size_t       getNumRows()        const noexcept { return _nrows; };
+        inline size_t       getNumCols()        const noexcept { return _ncols; };
+        inline size_t       getNumNonZeros()    const noexcept { return _numnz; };
+        virtual inline MatrixFormat getFormat() const noexcept { return _fmt; };
+
         void    setValue(const size_t i, const size_t j, fp_type val);
-        fp_type getValue(const size_t i, const size_t j);
+        fp_type getValue(const size_t i, const size_t j) const;
         virtual void assemble() =0; // The "=0" defines this class as abstract
+        virtual void matvec() =0;
     };
 }
 
