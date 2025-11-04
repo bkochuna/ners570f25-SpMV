ceea3afca71048f862a6462c10c75b3c78c165cb
Brendan Kochunas <bkochuna@umich.edu>
2025-11-04 11:22:42 -0500

    WIP: Adds initial unit test for COO
    Description:
    This adds some unit tests for SparseMatrix_COO. Specifically we test
     * constructors and destructors
     * setValue and getValue
    
    Tests exposed one consideration which is do we want to let clients
    add zeros as non-zeros?

---
+++ tests/COO.cpp
@@ -0,0 +1,138 @@
+
+#include "SpMV.hpp"
+#include "unit_test_framework.hpp"
+
+using namespace SpMV;
+
+template <typename T>
+TEST_CASE(constructor) 
+{
+  SparseMatrix_COO<T> A(2,3);
+  
+  ASSERT(A.getNumRows() == 2);
+  ASSERT(A.getNumCols() == 3);
+  ASSERT(A.getNumNonZeros() == 0);
+  ASSERT(A.getState() == MatrixState::initialized);
+  ASSERT(A.getFormat() == MatrixFormat::COO);
+} // constructor
+
+template <typename T>
+TEST_CASE(polymorhphic_constructor) 
+{
+  SparseMatrix<T>* ptr_A = new SparseMatrix_COO<T>(2,3);
+  
+  ASSERT(ptr_A->getNumRows() == 2);
+  ASSERT(ptr_A->getNumCols() == 3);
+  ASSERT(ptr_A->getNumNonZeros() == 0);
+  ASSERT(ptr_A->getState() == MatrixState::initialized);
+  ASSERT(ptr_A->getFormat() == MatrixFormat::COO);
+
+  delete(ptr_A);
+} // polymorphic constructor
+
+template <typename T>
+TEST_CASE(construct_zero) 
+{
+  SparseMatrix_COO<T> A(0,0);
+  
+  ASSERT(A.getNumRows() == 0);
+  ASSERT(A.getNumCols() == 0);
+  ASSERT(A.getNumNonZeros() == 0);
+  ASSERT(A.getState() == MatrixState::initialized);
+  ASSERT(A.getFormat() == MatrixFormat::COO);
+
+} // construct_zero
+
+template <typename T>
+TEST_SUITE(constructors) 
+{
+  TEST(constructor<T>);
+  TEST(polymorhphic_constructor<T>);
+  TEST(construct_zero<T>);
+} // constructors
+
+/*****************************************************************************/
+
+template <typename T>
+TEST_CASE(get_value_no_set)
+{
+  SparseMatrix_COO<T> A(2,2);
+
+  ASSERT_NEAR(A.getValue(0,0),static_cast<T>(0.0),TWO_EPS);
+
+} // get_value_no_set
+
+template <typename T>
+TEST_CASE(setget_square)
+{
+  SparseMatrix_COO<T> A(2,2);
+
+  A.setValue(0,0,1.0);
+  ASSERT(A.getNumNonZeros() == 1);
+  ASSERT(A.getState() == MatrixState::building);
+  A.setValue(1,0,2.0);
+  ASSERT(A.getNumNonZeros() == 2);
+  A.setValue(0,1,3.0);
+  ASSERT(A.getNumNonZeros() == 3);
+  A.setValue(1,1,4.0);
+  ASSERT(A.getNumNonZeros() == 4);
+
+  ASSERT_NEAR(A.getValue(0,0),static_cast<T>(1.0),TWO_EPS);
+  ASSERT_NEAR(A.getValue(1,0),static_cast<T>(2.0),TWO_EPS);
+  ASSERT_NEAR(A.getValue(0,1),static_cast<T>(3.0),TWO_EPS);
+  ASSERT_NEAR(A.getValue(1,1),static_cast<T>(4.0),TWO_EPS);
+
+} // setget_square
+
+template <typename T>
+TEST_CASE(set_zero)
+{
+  SparseMatrix_COO<T> A(2,2);
+
+  A.setValue(0,0,0.0);
+  ASSERT(A.getNumNonZeros() == 1);
+
+} // set_zero
+
+template <typename T>
+TEST_CASE(double_set)
+{
+  SparseMatrix_COO<T> A(2,2);
+
+  A.setValue(0,0,1.0);
+  ASSERT(A.getNumNonZeros() == 1);
+  A.setValue(0,0,2.0);
+  ASSERT(A.getNumNonZeros() == 1);
+  ASSERT_NEAR(A.getValue(0,0),static_cast<T>(2.0),TWO_EPS);
+
+} // double_set
+
+template <typename T>
+TEST_SUITE(setget_values) 
+{
+  TEST(get_value_no_set<T>);
+  TEST(setget_square<T>);
+  TEST(double_set<T>);
+  TEST(set_zero<T>);
+} // setget_values
+
+/*****************************************************************************/
+// for assemble
+
+/*****************************************************************************/
+// for matvec
+
+/*****************************************************************************/
+auto
+main() -> int
+{
+  // Run the unit tests. If a test fails, the program will print failure info
+  // and return 1.
+  RUN_SUITE(constructors<float>);
+  RUN_SUITE(constructors<double>);
+
+  RUN_SUITE(setget_values<float>);
+  RUN_SUITE(setget_values<double>);
+
+  return 0;
+}
