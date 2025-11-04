494babc0d42e1af67c36aec406bb40f9f64ddc93
Brendan Kochunas <bkochuna@umich.edu>
2025-11-04 09:49:18 -0500

    WIP: Updates several method implementations on SparseMatrix
    Description:
    This commit updates several of the method implementations on the
    SparseMatrix class:
     * Updates constructor to assign _fmt matrix format attribute and
       comments out debug statement.
     * Comments out debug statements in destructor.
     * Provides implementation for setValue.
     * Provides implementation for getValue.

--- src/SparseMatrix.cpp
+++ src/SparseMatrix.cpp
@@ -8,10 +8,10 @@ namespace SpMV
 
 template <class fp_type>
 SparseMatrix<fp_type>::SparseMatrix(const size_t nrows, const size_t ncols) :
-    _nrows(nrows), _ncols(ncols)
+    _nrows(nrows), _ncols(ncols), _fmt(MatrixFormat::COO)
 {
     assert(this->_state == MatrixState::undefined);
-    cout << "Hello from SparseMatrix Constructor!" << endl;
+    //cout << "Hello from SparseMatrix Constructor!" << endl;
     this->_state = MatrixState::initialized;
     assert(this->_state == MatrixState::initialized);
 }
@@ -19,19 +19,19 @@ SparseMatrix<fp_type>::SparseMatrix(const size_t nrows, const size_t ncols) :
 template <class fp_type>
 SparseMatrix<fp_type>::~SparseMatrix()
 {
-    cout << "Hello from SparseMatrix Destructor!" << endl;
-    cout << "this->_nrows=" << this->_nrows << endl;
-    cout << "this->_ncols=" << this->_ncols << endl;
-    cout << "this->_numnz=" << this->_numnz << endl;
+    // cout << "Hello from SparseMatrix Destructor!" << endl;
+    // cout << "this->_nrows=" << this->_nrows << endl;
+    // cout << "this->_ncols=" << this->_ncols << endl;
+    // cout << "this->_numnz=" << this->_numnz << endl;
     this->_state=MatrixState::undefined;
 }
 
 template <class fp_type>
 void SparseMatrix<fp_type>::setValue(const size_t i, const size_t j, fp_type val)
 {
-    assert(this->_state >= MatrixState::initialized);
-    assert(i < this->_nrows);
-    assert(j < this->_ncols);
+    assert(this->getState() >= MatrixState::initialized);
+    assert(i < this->getNumRows());
+    assert(j < this->getNumCols());
 
     this->_state = MatrixState::building;
 
@@ -39,20 +39,30 @@ void SparseMatrix<fp_type>::setValue(const size_t i, const size_t j, fp_type val
     this->_buildCoeff[std::make_pair(i,j)] = val;
     this->_numnz = this->_buildCoeff.size();
 
+    /*
     cout << "i=" << i << endl;
     cout << "j=" << j << endl;
     cout << "val=" << val << endl;
+    */
 
-    assert(this->_state == MatrixState::building);
+    assert(this->getState() == MatrixState::building);
 }
 
 template <class fp_type>
-fp_type SparseMatrix<fp_type>::getValue(const size_t i, const size_t j)
+fp_type SparseMatrix<fp_type>::getValue(const size_t i, const size_t j) const
 {
-    assert(this->_state > MatrixState::initialized);
+    assert(this->getState() >= MatrixState::initialized);
 
-    cout << "i=" << i << endl;
-    cout << "j=" << j << endl;
+    if(this->getState() == MatrixState::building) {
+        auto it = this->_buildCoeff.find(std::make_pair(i,j));
+        if(it != this->_buildCoeff.end()) {
+            return it->second;
+        } else {
+            return 0.0;
+        }
+    } else if (this->getState() == MatrixState::assembled) {
+        return this->_getValueConcrete(i,j);
+    }
     return 0.0;
 }
 
