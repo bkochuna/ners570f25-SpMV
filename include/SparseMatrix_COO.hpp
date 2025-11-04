fed79656027de376f05b3b3cf96cf2dca7decced
Brendan Kochunas <bkochuna@umich.edu>
2025-11-04 09:53:05 -0500

    WIP: Defines SparseMatrix_COO class and implementation
    Description:
    This commit adds the definitions for several members of the
    SparseMatrix_COO class and adds some of their implementations in
    SparseMatrix_COO.cpp. Therefore this commit does not build.
    It adds definitions for:
     * private methods _disassemble and _getValueConcrete which
       must be defined on derived classes
     * COO datatype attributes these are implemented as raw pointers
     * Defines _fmt for MatrixFormat
     * Defines public methods assemble and matvec which must be
       defined on derived class
    
    Not all routines are implemented. None are tested.
    Commit should build, but code may have bugs.

--- include/SparseMatrix_COO.hpp
+++ include/SparseMatrix_COO.hpp
@@ -8,9 +8,22 @@ namespace SpMV
     template <class fp_type>
     class SparseMatrix_COO : public SparseMatrix<fp_type>
     {
+        private:
+            // Abstract private methods from base class
+            void _disassemble() override;
+            fp_type _getValueConcrete(const size_t i, const size_t j) const override;
+
+            // Concrete storage for COO
+            size_t*    _I = nullptr;
+            size_t*    _J = nullptr;
+            fp_type* _val = nullptr;
+        
+            const MatrixFormat _fmt;
         public:
             SparseMatrix_COO(const size_t nrows, const size_t ncols);
-            void assemble();
+            ~SparseMatrix_COO() override;
+            void assemble() override;
+            void matvec() override;
     };
 }
 
--- src/SparseMatrix_COO.cpp
+++ src/SparseMatrix_COO.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <stddef.h>
+#include <cassert>
 
 using namespace std;
 
@@ -9,17 +10,102 @@ namespace SpMV
 {
     template <class fp_type>
     SparseMatrix_COO<fp_type>::SparseMatrix_COO(const size_t nrows, const size_t ncols) :
-        SparseMatrix<fp_type>::SparseMatrix(nrows, ncols)
+        SparseMatrix<fp_type>::SparseMatrix(nrows, ncols), _fmt(MatrixFormat::COO)
     {
-        cout << "Hello from COO Constructor" << endl;
+        //cout << "Hello from COO Constructor" << endl;
+    }
+
+    template <class fp_type>
+    SparseMatrix_COO<fp_type>::~SparseMatrix_COO()
+    {
+        if(this->_I != nullptr) {
+            delete[] this->_I;
+            this->_I = nullptr;
+        }
+
+        if(this->_J != nullptr) {
+            delete[] this->_J;
+            this->_J = nullptr;
+        }
+
+        if(this->_val != nullptr) {
+            delete[] this->_val;
+            this->_val = nullptr;
+        }
+
+        assert(this->_I == nullptr);
+        assert(this->_J == nullptr);
+        assert(this->_val == nullptr);
+    }
+
+    template <class fp_type>
+    void SparseMatrix_COO<fp_type>::_disassemble()
+    {
+        assert(this->getState() == MatrixState::assembled);
+        this->setState(MatrixState::building);
+
+        size_t nnz = this->getNumNonZeros();
+        for(size_t n = 0; n < nnz; n++) {
+            size_t    i = this->_I[n];
+            size_t    j = this->_J[n];
+            fp_type aij = this->_val[n];
+            this->setValue(i,j,aij);
+        }
+        
+        delete[] this->_I;   this->_I   = nullptr;
+        delete[] this->_J;   this->_J   = nullptr;
+        delete[] this->_val; this->_val = nullptr;
+
+        assert(this->getState() == MatrixState::building);
+        assert(this->_I   == nullptr);
+        assert(this->_J   == nullptr);
+        assert(this->_val == nullptr);
+    }
+
+    template <class fp_type>
+    fp_type SparseMatrix_COO<fp_type>::_getValueConcrete(const size_t i, const size_t j) const
+    {
+        assert(i < this->getNumRows());
+        assert(j < this->getNumCols());
+        assert(this->getState() == MatrixState::assembled);
+
+        for(size_t n; n < this->getNumNonZeros(); n++) {
+            if(i == this->_I[n] && j == this->_J[n])
+                return this->_val[n];
+        }
+
+        return 0.0;
     }
 
     template <class fp_type>
     void SparseMatrix_COO<fp_type>::assemble()
     {
-        cout << "Hello from COO assemble" << endl;
+        assert(this->getState() == MatrixState::building);
 
-        //This routine needs to convert _buildCoeff into the COO storage format.
+        if(this->getNumNonZeros() > 0)
+        this->_I   = new  size_t[this->getNumNonZeros()];
+        this->_J   = new  size_t[this->getNumNonZeros()];
+        this->_val = new fp_type[this->getNumNonZeros()];
+
+        size_t nnz = 0;
+        for(const auto& [key, val] : this->_buildCoeff) {
+            this->  _I[nnz] = key.first;
+            this->  _J[nnz] = key.second;
+            this->_val[nnz] = val;
+            nnz++;
+        }
+
+        this->_buildCoeff.clear();        
+        this->setState(MatrixState::assembled);
+
+        assert(this->getState() == MatrixState::assembled);
+        assert(this->_buildCoeff.empty());
+    }
+
+    template <class fp_type>
+    void SparseMatrix_COO<fp_type>::matvec()
+    {
+        //Not implemented yet
     }
 }
 
